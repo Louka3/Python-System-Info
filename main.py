9eb80b2f747fd2a9fb31840fb45196ebd82ce8cc
Louka3 <lkuczykowski@gmail.com>
2025-05-14 00:16:43 -0400

    obtained total memory and converted to GB

--- main.py
+++ main.py
@@ -4,14 +4,22 @@ import os as OS
 
 # OS, CPU, RAM, disk space, etc.
 
+BYTES_IN_GB = 1e9
+
+
 my_os = Platform.system()
 print(my_os) # OS
 my_uname = Platform.uname()
 print(my_uname)
 my_machine = Platform.machine()
 print(my_machine)
+my_cpu = Platform.processor()
+print(my_cpu)
 
+mem = PSUtil.virtual_memory()
+total_mem = mem.total / BYTES_IN_GB
 
+print(str(total_mem) + ' GB')
 
 
 

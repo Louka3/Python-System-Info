1d0b0c67ff58b65f25d5aa43e0b0b9cfab6a3da4
Louka3 <lkuczykowski@gmail.com>
2025-05-13 23:18:04 -0400

    adding gitignore, project objectives, and the start of main

---
+++ main.py
@@ -0,0 +1,17 @@
+import platform as Platform
+import psutil as PSUtil
+import os as OS
+
+# OS, CPU, RAM, disk space, etc.
+
+my_os = Platform.system()
+print(my_os) # OS
+my_uname = Platform.uname()
+print(my_uname)
+my_machine = Platform.machine()
+print(my_machine)
+
+
+
+
+

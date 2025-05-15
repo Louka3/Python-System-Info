475a2d5fe2b34e55cd0636461f76119d73995d3c
Louka3 <lkuczykowski@gmail.com>
2025-05-15 10:19:42 -0400

    trying to use the rich library to format the output in terminal better

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 1. System Info Dashboard (CLI)
 
-What it does: Prints your system info (OS, CPU, RAM, disk space, etc.)
-Skills: platform, psutil, CLI formatting
-Why: Great intro to IT scripting and working with system internals.
+What it does: 
+Prints your system info (OS, CPU, RAM, disk space, etc.)
+
+Skills: 
+platform, psutil, CLI formatting
+
+Why: 
+Great intro to IT scripting and working with system internals.
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import platform as Platform
 import psutil as PSUtil
 import os as OS
+from rich import print as rprint
+from rich.columns import Columns
 
 # OS, CPU, RAM, disk space, etc.
 
@@ -8,18 +10,24 @@ BYTES_IN_GB = 1e9
 
 
 my_os = Platform.system()
-print(my_os) # OS
+
 my_uname = Platform.uname()
-print(my_uname)
+# print(my_uname)
 my_machine = Platform.machine()
-print(my_machine)
+
 my_cpu = Platform.processor()
-print(my_cpu)
+# print(my_cpu)
 
 mem = PSUtil.virtual_memory()
 total_mem = mem.total / BYTES_IN_GB
 
-print(str(total_mem) + ' GB')
+ds = PSUtil.disk_usage('/')
 
 
+print("OS: " + my_os)
+print("CPU: " + my_cpu)
+print("RAM: " + format(total_mem, '.3f') + " GB")
+print("Disk Space:")
+print("\tTotal: " + format(ds.total / BYTES_IN_GB, '.3f') + " GB\n\tUsed: " + format(ds.used / BYTES_IN_GB, '.3f') + " GB\n\tFree: "+ format(ds.free / BYTES_IN_GB, '.3f') + " GB") 
 
+rprint(Columns([my_os, my_cpu, my_machine], padding=(3,3)))
